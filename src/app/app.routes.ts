f4a6bacaf06442ca76a3c0da108037b5ea0e3e4b
hugo vilar <hugovilar04@gmail.com>
2025-11-04 15:04:12 -0300

    FEAT: adidiconando rotas com lazyLoadin

---
+++ src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./components/inicio/inicio.component').then(c => c.InicioComponent)
+  },
+  {
+    path: 'geral',
+    loadChildren: () => import('./components/geral/geral.routes').then(m => m.routes)
+  },
+  {
+    path:'transacoes',
+    loadChildren: () => import('./components/transacoes/transacoes.routes').then(m => m.routes)
+  }
+];
