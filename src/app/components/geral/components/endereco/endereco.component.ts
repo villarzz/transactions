52e6cfc6ce2a23225d7bb2300cac2d14b140fdd1
hugo vilar <hugovilar04@gmail.com>
2025-11-04 15:05:29 -0300

    FEAT: Implementar componente Endereco com HTML, SCSS

---
+++ src/app/components/geral/components/endereco/endereco.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-endereco',
+  standalone: true,
+  imports: [],
+  templateUrl: './endereco.component.html',
+  styleUrl: './endereco.component.scss'
+})
+export class EnderecoComponent {
+
+}
